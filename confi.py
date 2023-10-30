a9dd6c1d280b2bc3010211bfbd820ed67bbc7570
pedro1287 <102392245+pedro1287@users.noreply.github.com>
2023-10-29 20:29:38 -0400

    Update confi.py

--- confi.py
+++ confi.py
@@ -1,3 +1,3 @@
-Bot_token= "5823282217:AAHQYY5Yu1g6k6_mzrxXYnCjEoNP_ar0WXQ"
+Bot_token= "6700655023:AAFTV7qrvdUAJNyjTYLj_V11JXabgTMdVag"
 chanel_id =  -831778463
 db_access = 2 #value int de la db, desmarcar line 1531 bot.py
